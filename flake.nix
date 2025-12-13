9f73849c306462038f86076a5f00fcc00f6e591c
Regalk <72028266+regalk13@users.noreply.github.com>
2025-12-13 14:27:45 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "XM LAB";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, android-nixpkgs }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+
+        androidSdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+          cmdline-tools-latest
+          build-tools-34-0-0
+          platform-tools
+          platforms-android-34
+          emulator
+          sources-android-34
+        ]);
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "kmp-dev-shell";
+
+          buildInputs = with pkgs; [
+            jdk17
+
+            androidSdk
+
+            android-studio
+
+            gradle
+          ];
+
+          ANDROID_HOME = "${androidSdk}/share/android-sdk";
+          ANDROID_SDK_ROOT = "${androidSdk}/share/android-sdk";
+          JAVA_HOME = pkgs.jdk17.home;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+          ];
+        };
+      }
+    );
+}
